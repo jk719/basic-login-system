4165c8577761725a86635f021581d10aa7420e2e
Jamil Kabir <jamilkabir719@gmail.com>
2023-10-17 16:53:57 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const pool = require('./database');
+const path = require('path');
+
+const app = express();
+
+// Middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'secret key',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    pool.query('SELECT id, username, password FROM users WHERE username = $1', [username], (err, result) => {
+      if (err) return done(err);
+      const user = result.rows[0];
+      if (!user) return done(null, false);
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) return done(err);
+        if (isMatch) return done(null, { id: user.id, username: user.username });
+        else return done(null, false);
+      });
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  pool.query('SELECT id, username FROM users WHERE id = $1', [id], (err, result) => {
+    if (err) return done(err);
+    const user = result.rows[0];
+    if (!user) return done(null, false);
+    return done(null, user);
+  });
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/dashboard',
+  failureRedirect: '/login'
+}));
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 10);
+  pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id', [username, hash], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Server Error");
+    }
+    res.redirect('/login');
+  });
+});
+
+app.get('/dashboard', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.send(`Welcome, ${req.user.username}!`);
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get('/', (req, res) => {
+  res.redirect('/login');
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
